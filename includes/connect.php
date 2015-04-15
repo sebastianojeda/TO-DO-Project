4793903dfeb2f68b27ca8089bb8d7580583f9c0a
sebastianojeda <sebastianojeda.school@gmail.com>
2015-04-14 19:48:25 -0700

    Moved my connect file in a new folder called includes

--- includes/connect.php
+++ includes/connect.php
@@ -1,6 +1,6 @@
 <?php
 //why is this important? why does localhost have to be first?
-$mysqli = new mysqil('localhost', 'root', 'tasks');
+$mysqli= new mysqli('localhost', 'root', 'root', 'tasks');
 
 //$mysqli->connect_error then we want it to die and have error message pop up
 if($mysqli->connect_error){
