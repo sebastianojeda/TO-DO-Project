128aa12d19c02b65d497f51cac32144be3a22eed
sebastianojeda <sebastianojeda.school@gmail.com>
2015-04-15 12:07:33 -0700

    Done with vid 10 added some jquery to my index.php.

--- includes/connect.php
+++ includes/connect.php
@@ -1,6 +1,6 @@
 <?php
 //why is this important? why does localhost have to be first?
-$mysqli= new mysqli('localhost', 'root', 'root', 'tasks');
+$mysqli= new mysqli('localhost', 'root', 'root', 'to-do list');
 
 //$mysqli->connect_error then we want it to die and have error message pop up
 if($mysqli->connect_error){
--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
 	<title> Simple To-Do List </title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
 </head>
 <body>
 
@@ -14,8 +16,26 @@
 		</div>
 	</div>
 	<form class="add-new-task" autocomplete="off">
-		<input type="text" name="new-task" placeholder="Add new item..."/>
-			
+		<input type="text" name="new-task" placeholder="Add new item..."/>			
 	</form>
+
 </body>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script>
+	add_task();//calling add task function
+	//This is my add_task function
+	function add_task(){
+		$('.add-new-task').submit(function(){
+			var new_task = $('.add-new-task input[name=new-task]').val();
+			//This if statement is ckecking if new_task is true or not  
+			if(new_task != ''){
+				$.post('includes/add-task.php', { task: new_task}, function(data){
+					$(('add-new-task input[name=new-task]').val();
+						$(data).appendTo('task-list ul').hide().fadeIn();
+				});
+			}
+			return false;
+		});
+	}
+</script>
 </html>
