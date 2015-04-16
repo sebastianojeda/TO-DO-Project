40f174703067a3512bd9a9f2cc9c347c461ed665
sebastianojeda <sebastianojeda.school@gmail.com>
2015-04-16 12:13:30 -0700

    done with video 16

--- index.php
+++ index.php
@@ -11,8 +11,10 @@
 	<div class="wrap">
 		<div class="task-list">
 			<ul>
+	<!-- this is all server side code  -->
 				<?php require("includes/connect.php"); 
 				$mysqli = new mysqli('localhost', 'root', 'root', 'to-do list');
+				//this query is ordering a task by date and time
 				$query = 'SELECT * FROM tasks ORDER BY date ASC, time ASC';
 				if($result = $mysqli->query($query)){
 					$numrows = $result->num_rows;
@@ -21,13 +23,15 @@
 							$task_id = $row['id'];
 							$task_name = $row["task"];
 
-							echo "<li>
-							<span>'.$task_name'
-							";
+							echo '<li>
+							<span>'.$task_name.'</span>
+							<img id="'.$task_id.'" class="delete-button" width="10px" src="images/close.svg"/>
+							</li>';
 						}
 					}
 				}
 				?>
+			<!-- End of server side code -->
 			</ul>
 		</div>
 	</div>
