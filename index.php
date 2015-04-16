2ee54da3249ab12cf8742e518c748f11aac1d2e0
sebastianojeda <sebastianojeda.school@gmail.com>
2015-04-16 12:04:41 -0700

    done with vid 15 all we did was add some more code to my index,php file.

--- index.php
+++ index.php
@@ -11,7 +11,23 @@
 	<div class="wrap">
 		<div class="task-list">
 			<ul>
-				<?php require("includes/connect.php"); ?>
+				<?php require("includes/connect.php"); 
+				$mysqli = new mysqli('localhost', 'root', 'root', 'to-do list');
+				$query = 'SELECT * FROM tasks ORDER BY date ASC, time ASC';
+				if($result = $mysqli->query($query)){
+					$numrows = $result->num_rows;
+					if($numrows>0){
+						while($row = $result->fetch_assoc()){
+							$task_id = $row['id'];
+							$task_name = $row["task"];
+
+							echo "<li>
+							<span>'.$task_name'
+							";
+						}
+					}
+				}
+				?>
 			</ul>
 		</div>
 	</div>
