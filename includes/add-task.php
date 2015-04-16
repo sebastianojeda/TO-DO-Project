aae0e59c08b3ea652c034d784692fb99e2c6bb47
sebastianojeda <sebastianojeda.school@gmail.com>
2015-04-15 21:04:34 -0700

    finished vid 12. I added some php code into my add-task.php file. The code is setting my date and time and its also connecting to my database.

--- includes/add-task.php
+++ includes/add-task.php
@@ -11,6 +11,15 @@
 
 	$query = "SELECT * FROM tasks WHERE task='$task' and date='$date' and time='$time' ";
 
+	if($result = $mysqli->query($query)) {
+		while($row = $result->fetch_assoc()){
+			$task_id = $row['id'];
+			$task_name = $row['task'];
+		}
+	}
 
+	$mysqli->close();
+
+	echo '<li><span>'.$task_name.'</span><img id="'.$task_id.'" class="delete-button" width="10px" src="images/close.svg"/></li>';
 
 ?>
